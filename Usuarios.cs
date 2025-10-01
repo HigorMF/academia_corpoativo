19ed1e13224697bdb10eddae45970b21d591b87c
nihfelz <nicolefelizardo970@gmail.com>
2025-09-30 21:40:12 -0300

    fgdgfd

---
+++ Usuarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace academia_corpoativo
+{
+    public class Usuarios
+    {
+        public string Usuario { get; set;}
+        public string Senha { get; set; }
+
+        public Usuarios (string usuario, string senha)
+        {
+            Usuario = usuario;
+            Senha = senha;
+        }
+    }
+}
