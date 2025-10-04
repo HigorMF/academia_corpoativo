d11fc2eeb4a4e7808924bdcc4f7d000c804a65a4
nihfelz <nicolefelizardo970@gmail.com>
2025-10-03 21:04:15 -0300

    dsfdf

--- Aluno.Designer.cs
+++ Aluno.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Aluno";
+            SuspendLayout();
+            // 
+            // Aluno
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(918, 641);
+            Name = "Aluno";
+            Text = "Aluno";
+            ResumeLayout(false);
         }
 
         #endregion
--- Professor.Designer.cs
+++ Professor.Designer.cs
@@ -28,12 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Professor";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Professor));
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            pictureBox7 = new PictureBox();
+            label3 = new Label();
+            label4 = new Label();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Black;
+            pictureBox1.Location = new Point(16, 183);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(311, 415);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(364, 183);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(519, 415);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = SystemColors.ActiveCaptionText;
+            pictureBox3.Location = new Point(389, 256);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(113, 143);
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = SystemColors.ActiveCaptionText;
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(25, 198);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(292, 254);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 5;
+            pictureBox6.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(16, 165);
+            label1.Name = "label1";
+            label1.Size = new Size(109, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Matrícula Professor";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(364, 165);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 7;
+            label2.Text = "label2";
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = SystemColors.ActiveBorder;
+            pictureBox7.Location = new Point(497, 256);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(360, 143);
+            pictureBox7.TabIndex = 8;
+            pictureBox7.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(389, 420);
+            label3.Name = "label3";
+            label3.Size = new Size(152, 15);
+            label3.TabIndex = 11;
+            label3.Text = "Agendamentos Registrados";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(389, 238);
+            label4.Name = "label4";
+            label4.Size = new Size(117, 15);
+            label4.TabIndex = 12;
+            label4.Text = "Frequência Professor";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = SystemColors.ActiveCaptionText;
+            pictureBox4.Location = new Point(389, 438);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(113, 143);
+            pictureBox4.TabIndex = 13;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = SystemColors.ActiveBorder;
+            pictureBox5.Location = new Point(497, 438);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(360, 143);
+            pictureBox5.TabIndex = 14;
+            pictureBox5.TabStop = false;
+            // 
+            // Professor
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(924, 645);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox7);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox2);
+            Name = "Professor";
+            Text = "Professor";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox6;
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox7;
+        private Label label3;
+        private Label label4;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
     }
 }
