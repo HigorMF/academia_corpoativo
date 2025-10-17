6caeb9567e9eba09b16aedc32ee70f6e00fa5ea8
nihfelz <nicolefelizardo970@gmail.com>
2025-10-16 21:56:34 -0300

    Up Incio

--- Tela_Inicial.Designer.cs
+++ Tela_Inicial.Designer.cs
@@ -48,6 +48,9 @@
             panel8 = new Panel();
             btnSobre = new Button();
             sidebarTime = new System.Windows.Forms.Timer(components);
+            HomeContainer = new Panel();
+            btnSubMenu = new Button();
+            HomeTime = new System.Windows.Forms.Timer(components);
             sidebar.SuspendLayout();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
@@ -56,16 +59,13 @@
             panel3.SuspendLayout();
             panel5.SuspendLayout();
             panel7.SuspendLayout();
+            HomeContainer.SuspendLayout();
             SuspendLayout();
             // 
             // sidebar
             // 
             sidebar.BackColor = Color.Black;
             sidebar.Controls.Add(panel1);
-            sidebar.Controls.Add(panel9);
-            sidebar.Controls.Add(panel3);
-            sidebar.Controls.Add(panel5);
-            sidebar.Controls.Add(panel7);
             sidebar.Dock = DockStyle.Left;
             sidebar.Location = new Point(0, 0);
             sidebar.MaximumSize = new Size(223, 570);
@@ -116,7 +116,7 @@
             // 
             panel9.Controls.Add(panel10);
             panel9.Controls.Add(button4);
-            panel9.Location = new Point(3, 168);
+            panel9.Location = new Point(298, 230);
             panel9.Name = "panel9";
             panel9.Size = new Size(220, 56);
             panel9.TabIndex = 6;
@@ -130,26 +130,28 @@
             // 
             // button4
             // 
+            button4.BackColor = Color.Black;
             button4.FlatStyle = FlatStyle.Flat;
             button4.Font = new Font("Schadow BT", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button4.ForeColor = Color.White;
             button4.Image = Properties.Resources.imagem;
             button4.ImageAlign = ContentAlignment.TopLeft;
-            button4.Location = new Point(-26, 3);
+            button4.Location = new Point(-28, 0);
             button4.Name = "button4";
             button4.Padding = new Padding(30, 0, 0, 0);
-            button4.Size = new Size(255, 56);
+            button4.Size = new Size(255, 57);
             button4.TabIndex = 3;
             button4.Text = "              Inicio";
             button4.TextAlign = ContentAlignment.TopLeft;
-            button4.UseVisualStyleBackColor = true;
+            button4.UseVisualStyleBackColor = false;
             button4.Click += button4_Click;
             // 
             // panel3
             // 
+            panel3.BackColor = SystemColors.ActiveCaptionText;
             panel3.Controls.Add(panel4);
             panel3.Controls.Add(bntLogin);
-            panel3.Location = new Point(3, 230);
+            panel3.Location = new Point(618, 168);
             panel3.Name = "panel3";
             panel3.Size = new Size(220, 56);
             panel3.TabIndex = 7;
@@ -166,7 +168,7 @@
             bntLogin.FlatStyle = FlatStyle.Flat;
             bntLogin.Font = new Font("Schadow BT", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             bntLogin.ForeColor = Color.White;
-            bntLogin.Image = Properties.Resources.Design_sem_nome;
+            bntLogin.Image = Properties.Resources.imagem;
             bntLogin.ImageAlign = ContentAlignment.TopLeft;
             bntLogin.Location = new Point(-26, 0);
             bntLogin.Name = "bntLogin";
@@ -179,9 +181,10 @@
             // 
             // panel5
             // 
+            panel5.BackColor = SystemColors.ActiveCaptionText;
             panel5.Controls.Add(panel6);
             panel5.Controls.Add(btnCadastro);
-            panel5.Location = new Point(3, 292);
+            panel5.Location = new Point(618, 230);
             panel5.Name = "panel5";
             panel5.Size = new Size(220, 56);
             panel5.TabIndex = 8;
@@ -211,9 +214,10 @@
             // 
             // panel7
             // 
+            panel7.BackColor = SystemColors.ActiveCaptionText;
             panel7.Controls.Add(panel8);
             panel7.Controls.Add(btnSobre);
-            panel7.Location = new Point(3, 354);
+            panel7.Location = new Point(618, 292);
             panel7.Name = "panel7";
             panel7.Size = new Size(220, 56);
             panel7.TabIndex = 9;
@@ -244,7 +248,37 @@
             // 
             // sidebarTime
             // 
+            sidebarTime.Interval = 10;
             sidebarTime.Tick += sidebarTime_Tick;
+            // 
+            // HomeContainer
+            // 
+            HomeContainer.BackColor = Color.Black;
+            HomeContainer.Controls.Add(btnSubMenu);
+            HomeContainer.Location = new Point(298, 230);
+            HomeContainer.Name = "HomeContainer";
+            HomeContainer.Size = new Size(220, 102);
+            HomeContainer.TabIndex = 7;
+            // 
+            // btnSubMenu
+            // 
+            btnSubMenu.BackColor = SystemColors.WindowFrame;
+            btnSubMenu.FlatStyle = FlatStyle.Flat;
+            btnSubMenu.Font = new Font("Schadow BT", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSubMenu.ForeColor = Color.White;
+            btnSubMenu.ImageAlign = ContentAlignment.TopLeft;
+            btnSubMenu.Location = new Point(-28, 54);
+            btnSubMenu.Name = "btnSubMenu";
+            btnSubMenu.Padding = new Padding(30, 0, 0, 0);
+            btnSubMenu.Size = new Size(255, 56);
+            btnSubMenu.TabIndex = 4;
+            btnSubMenu.Text = "              Sub Menu";
+            btnSubMenu.TextAlign = ContentAlignment.TopLeft;
+            btnSubMenu.UseVisualStyleBackColor = false;
+            // 
+            // HomeTime
+            // 
+            HomeTime.Tick += HomeTime_Tick;
             // 
             // Tela_Inicial
             // 
@@ -253,6 +287,11 @@
             BackColor = SystemColors.ButtonHighlight;
             ClientSize = new Size(1095, 570);
             Controls.Add(sidebar);
+            Controls.Add(panel9);
+            Controls.Add(panel3);
+            Controls.Add(HomeContainer);
+            Controls.Add(panel7);
+            Controls.Add(panel5);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Tela_Inicial";
             Text = "Tela_Inicial";
@@ -266,6 +305,7 @@
             panel3.ResumeLayout(false);
             panel5.ResumeLayout(false);
             panel7.ResumeLayout(false);
+            HomeContainer.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -275,8 +315,6 @@
         private Panel panel9;
         private Panel panel10;
         private Button button4;
-        private Panel panel1;
-        private Panel panel2;
         private Panel panel3;
         private Panel panel4;
         private Button bntLogin;
@@ -286,8 +324,13 @@
         private Panel panel7;
         private Panel panel8;
         private Button btnSobre;
+        private System.Windows.Forms.Timer sidebarTime;
+        private Panel panel1;
+        private Panel panel2;
         private Label label1;
         private PictureBox btnBotao;
-        private System.Windows.Forms.Timer sidebarTime;
+        private Panel HomeContainer;
+        private Button btnSubMenu;
+        private System.Windows.Forms.Timer HomeTime;
     }
 }
